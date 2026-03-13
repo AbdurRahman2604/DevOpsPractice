25cbf86afe0669ab7c502014f9ccb1d7c753dcf8
AbdurRahman2604 <arr2604@gmail.com>
2026-03-13 15:14:45 +0530

    updated code for CI Jenkins

--- DevOpsProgramInPython.py
+++ DevOpsProgramInPython.py
@@ -1,6 +1,12 @@
 a=10;
 b=100;
 c=20;
+d=25;
 print(a);
 print(b);
 print(c);
+print(d);
+print("CI pipeline triggered successfully")
+print("CI pipeline triggered successfully")
+print("CI pipeline triggered successfully")
+print("CI pipeline triggered successfully")
